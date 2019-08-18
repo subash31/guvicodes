707f5e2e627afbd2136973031efd2e52da6c681e
subash31 <52916416+subash31@users.noreply.github.com>
2019-08-18 10:57:38 +0530

    Update firstnmul.py

--- firstnmul.py
+++ firstnmul.py
@@ -1,3 +1,6 @@
 def printMultiples(n, m):
+'takes n and m as integers and finds all first m multiples of n'
 for m in (n,m):
-    print(n, end = ' ')
+    if n % 2 == 0:
+        while n < 0:
+            print(n)
